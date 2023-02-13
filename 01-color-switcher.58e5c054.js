f8a0c44d4330dc31a5136bf958daccfb74650772
dburdin <113050865+dburdin@users.noreply.github.com>
2023-02-13 12:40:39 +0000

    Deploying to gh-pages from @ dburdin/goit-js-hw-09@992967688f56d167a647d2de1b188d33ba8c8300 🚀

---
+++ 01-color-switcher.58e5c054.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.58e5c054.js.map
